7ec85f44725a130e8dc5e275263a5d7b3d448984
yagmurdal <nyagmurdal@gmail.com>
2022-01-16 17:50:37 +0300

    pagination added

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const api = "http://www.omdbapi.com/?apikey=780a60dd&s=";
 const searchInput = document.querySelector("#search");
 const resultBox = document.querySelector(".result-box .row");
+const paginationBox = document.querySelector(".pagination");
 
 async function getMovies(title, pageNumber) {
   let { data } = await axios.get(`${api}${title}&page=${pageNumber}`);
@@ -24,7 +25,22 @@ async function getMovies(title, pageNumber) {
         </div>
       `;
   });
-  searchInput.value = "";
+  let pageCount = data.totalResults / 10;
+  paginationBox.innerHTML = "";
+  for (let i = 0; i < pageCount; i++) {
+    const button = document.createElement("button");
+    button.innerText = i + 1;
+    button.id = "button-" + (i + 1);
+    button.setAttribute("index", i + 1);
+    button.addEventListener("click", buttonClicked(i));
+    paginationBox.appendChild(button);
+  }
+
+  function buttonClicked(index) {
+    return function () {
+      getMovies(searchInput.value, index + 1);
+    };
+  }
 }
 
 searchInput.addEventListener("keyup", function (e) {
@@ -32,3 +48,13 @@ searchInput.addEventListener("keyup", function (e) {
     getMovies(e.target.value);
   }
 });
+
+function myfunc() {
+  console.log(1);
+}
+
+// TO-DO'S
+// searh-inputa tıklandığında içerisindeki değer silenecek.
+// çıkacak bug'ı çöz. silindikten sonra boş string döneceği için pag. patlayabilir
+// pag. işleminden sonra sayfanın yukarıya scrool edilmesi
+// pag.'nın ui'nın düzenlenmesi
